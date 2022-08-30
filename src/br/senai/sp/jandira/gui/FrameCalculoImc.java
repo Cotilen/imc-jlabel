cc38336064da959fa4cd5543b8c2c321aca0ca08
Cleiton <cleitonncruz105@gmail.com>
2022-08-30 08:26:25 -0300

    Jlabel completo

--- src/br/senai/sp/jandira/gui/FrameCalculoImc.java
+++ src/br/senai/sp/jandira/gui/FrameCalculoImc.java
@@ -7,6 +7,7 @@ import java.awt.Font;
 import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
@@ -137,7 +138,7 @@ public class FrameCalculoImc {
 				
 			}
 		});
-
+		
 		
 		
 		
